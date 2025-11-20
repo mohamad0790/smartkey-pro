21a2be188fb999ef5731528558665515dc8c31f7
mohamad0790 <mohamadsalah2035@gmail.com>
2025-11-20 06:44:15 +0300

    Delete pages/supabase.js

--- pages/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-export const supabase = createClient(
-  "https://qvnxhqqewluqcdddltiw.supabase.co",
-  "sb_publishable_nHJOtV9xzyJb8xJ3Wexx0S32EwQ2k2nFf"
-);
