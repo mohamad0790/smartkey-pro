1b1534f5f81738b30d42027ad2e343280184cd1b
mohamad0790 <mohamadsalah2035@gmail.com>
2025-11-20 07:07:27 +0300

    Delete pages/supabase.js

--- pages/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-
-// ضع الرابط والمفتاح الخاص بحسابك هنا
-const supabaseUrl = "https://qvnxhqqewluqcdddltiw.supabase.co";
-const supabaseKey = "اكتب_المفتاح_كامل_هنا";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
