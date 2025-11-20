4b98cfd91456e466cb7eefe96e1dc5a2202396f3
mohamad0790 <mohamadsalah2035@gmail.com>
2025-11-20 07:09:48 +0300

    Add Supabase client initialization

---
+++ pages/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
+  "https://qvnxhqqewluqcdddltiw.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2bnhocXFld2x1cWNkZGRsdGl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMzMwMTMsImV4cCI6MjA3ODcwOTAxM30.5aw42Tx5Dj3ws4xHpErGYAulxN0OclPa3prDx_e_yR0"
+);
