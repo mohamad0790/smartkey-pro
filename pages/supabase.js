df74113f9f5d27bac3e07541b69748998dfd627c
mohamad0790 <mohamadsalah2035@gmail.com>
2025-11-21 08:13:43 +0300

    Update Supabase client to version 2

--- pages/supabase.js
+++ pages/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 export const supabase = createClient(
   "https://qvnxhqqewluqcdddltiw.supabase.co",
