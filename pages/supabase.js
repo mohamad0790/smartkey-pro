318a517db8f79805e19fe74dec89400c7a17caf1
mohamad0790 <mohamadsalah2035@gmail.com>
2025-11-24 05:53:26 +0300

    Update supabase.js

--- pages/supabase.js
+++ pages/supabase.js
@@ -1,10 +1,1 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-// رابط المشروع
-const SUPA_URL = "https://qvnxhqqewluqcdddltiw.supabase.co";
-
-// مفتاح الـ API كامل بدون أي نقص
-const SUPA_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2bnhocXFld2x1cWNkZGRsdGl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMzMwMTMsImV4cCI6MjA3ODcwOTAxM30.5aw42Tx5Dj3ws4xHpErGYAulxN0OclPa3prDx_e_yR0";
-
-// إنشاء الاتصال
-export const supabase = createClient(SUPA_URL, SUPA_KEY);
