07955b71b2c99600c6c9c934113217dde836c2d4
mohamad0790 <mohamadsalah2035@gmail.com>
2025-11-28 13:12:03 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -5,5 +5,5 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const SUPA_URL = "https://qvnxhqqewluqcdddltiw.supabase.co";
 const SUPA_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2bnhocXFld2x1cWNkZGRsdGl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMzMwMTMsImV4cCI6MjA3ODcwOTAxM30.5aw42Tx5Dj3ws4xHpErGYAulxN0OclPa3prDx_e_yR0";
 
-// تصدير Supabase جاهز
+// إنشاء Supabase
 export const supabase = createClient(SUPA_URL, SUPA_ANON_KEY);
